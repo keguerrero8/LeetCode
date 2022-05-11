266a0077b3f88651632e3ee571d6b9545a95f4f4
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-05-11 14:01:56 -0400

    Time: 22 ms (67.78%), Space: 13.5 MB (37.34%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,26 +1,26 @@
 class Solution(object):
-    # def fib(self, n, hashMap = None):
-    #     if hashMap == None:
-    #         hashMap = {0:0, 1:1}
-    #     if n in hashMap:
-    #         return hashMap[n]
-    #     else:
-    #         hashMap[n] = fib(n-1, hashMap) + fib(n-2, hashMap)
-    #     return hashMap[n]
+    def fib(self, n, hashMap = None):
+        if hashMap == None:
+            hashMap = {0:0, 1:1}
+        if n in hashMap:
+            return hashMap[n]
+        else:
+            hashMap[n] = self.fib(n-1, hashMap) + self.fib(n-2, hashMap)
+        return hashMap[n]
     
-    def fib(self, n):
-        fibNums = [0, 1]
+#     def fib(self, n):
+#         fibNums = [0, 1]
         
-        if n == 1:
-            return 1
-        elif n == 0:
-            return 0
+#         if n == 1:
+#             return 1
+#         elif n == 0:
+#             return 0
         
-        for i in range(2, n+1):
-            currentFib = fibNums[0] + fibNums[1]
-            fibNums[0] = fibNums[1]
-            fibNums[1] = currentFib
+#         for i in range(2, n+1):
+#             currentFib = fibNums[0] + fibNums[1]
+#             fibNums[0] = fibNums[1]
+#             fibNums[1] = currentFib
             
-        return currentFib
+#         return currentFib
         
         
