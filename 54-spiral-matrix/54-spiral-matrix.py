7c471c6a8c65e4064e1c6ae3465f98af6684f4f3
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-05-13 15:18:52 -0400

    Time: 32 ms (17.36%), Space: 13.8 MB (6.15%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -1,30 +1,39 @@
 class Solution(object):
-    def spiralOrder(self, array):
-        startRow = 0
-        startColumn = 0
-        endRow = len(array)-1
-        endColumn = len(array[0])-1
-        oneDArray = []
-        while startRow <= endRow and startColumn <= endColumn:
-            for i in range(startColumn, endColumn + 1):
-                oneDArray.append(array[startRow][i])
-
-            for i in range(startRow + 1, endRow + 1):
-                oneDArray.append(array[i][endColumn])
-
-            for i in range(endColumn-1, startColumn, -1):
-                if startRow == endRow:
+    def spiralOrder(self, matrix):
+        l = 0
+        r = len(matrix[0]) - 1
+        top = 0
+        bottom = len(matrix) - 1
+        result = []
+        
+        while l <= r and top <= bottom:
+            i = l
+            while i <= r:
+                result.append(matrix[top][i])
+                i += 1
+                
+            i = top + 1
+            while i < bottom:
+                result.append(matrix[i][r])
+                i += 1
+                
+            i = r
+            while i >= l:
+                if bottom == top:
                     break
-                oneDArray.append(array[endRow][i])
-
-            for i in range(endRow, startRow, -1):
-                if startColumn == endColumn:
+                result.append(matrix[bottom][i])
+                i -= 1
+                
+            i = bottom - 1
+            while i > top:
+                if l == r:
                     break
-                oneDArray.append(array[i][startColumn])
-
-            startRow += 1
-            startColumn += 1
-            endRow -= 1
-            endColumn -= 1
-
-        return oneDArray
+                result.append(matrix[i][l])
+                i -= 1
+                
+            l += 1
+            r -= 1
+            top += 1
+            bottom -= 1
+            
+        return result
