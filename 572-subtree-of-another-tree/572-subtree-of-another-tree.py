2a20380321bd1d65f6f160a77d47171d49e09ee3
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-05-24 10:02:36 -0400

    Time: 192 ms (36.83%), Space: 14.4 MB (57.28%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.py
@@ -17,10 +17,6 @@ class Solution(object):
     def isSameTree(self, root, subRoot):
         if root is None and subRoot is None:
             return True
-        elif root is None and subRoot is not None:
-            return False
-        elif root is not None and subRoot is None:
-            return False
         
         if root and subRoot and root.val == subRoot.val:
             return self.isSameTree(root.left, subRoot.left) and self.isSameTree(root.right, subRoot.right)
