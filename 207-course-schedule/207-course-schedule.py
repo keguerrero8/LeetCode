dcee8fe376696bf885048fb71a315abb794ca1cf
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-05-26 14:11:46 -0400

    Time: 107 ms (47.66%), Space: 16 MB (59.10%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,42 +1,44 @@
 class Solution(object):
     def canFinish(self, numCourses, prerequisites):
-        def dFS(course, prereqHash, visiting, canBeCompleted):
-            if course not in visiting or visiting[course] == False:
-                visiting[course] = True
-            elif visiting[course] == True:
-                return False
-                
-            if course not in prereqHash or course in canBeCompleted:
-                visiting[course] = False
-                return True
-            
-            for prereq in prereqHash[course]:
-                isPossible = dFS(prereq, prereqHash, visiting, canBeCompleted)
-                if not isPossible:
-                    return False
-                    
-            canBeCompleted[course] = True      
-            visiting[course] = False
-                
-            return True
-        
-        canBeCompleted = {}
+        # [[1, 0], [1, 2], [2, 0]], {1:[0, 2], 2:[0], 3:[1]}
         prereqHash = {}
-        for prereq in prerequisites:
-            if prereq[0] not in prereqHash:
-                prereqHash[prereq[0]] = [prereq[1]]
+        for i in prerequisites:
+            if i[0] not in prereqHash:
+                prereqHash[i[0]] = [i[1]]
             else:
-                prereqHash[prereq[0]].append(prereq[1])
+                prereqHash[i[0]].append(i[1])
                 
-        visiting = {}
-        isPossible = True
+        visiting = set()
+        possibleCourses = set()
         
         for course in range(numCourses):
-            isPossible = dFS(course, prereqHash, visiting, canBeCompleted)
+            isPossible = self.dFS(course, prereqHash, visiting, possibleCourses)
             if not isPossible:
                 return False
-            canBeCompleted[course] = True
-        
+            
         return True
     
     
+    def dFS(self, course, prereqHash, visiting, possibleCourses):
+        if course in visiting:
+            return False
+        
+        if course not in prereqHash or course in possibleCourses:
+            return True
+        
+        visiting.add(course)
+        
+        for prereq in prereqHash[course]:
+            isPossible = self.dFS(prereq, prereqHash, visiting, possibleCourses)
+            if not isPossible:
+                return False
+            
+        possibleCourses.add(course)
+        visiting.remove(course)
+        
+        return True
+        
+        
+            
+        
+        
