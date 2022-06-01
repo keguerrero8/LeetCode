85461f889894d0bb61d502afc3a06ab97c3328cf
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-06-01 15:13:31 -0400

    Time: 30 ms (39.69%), Space: 13.6 MB (72.63%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,20 +1,26 @@
 class Solution(object):
-    def subsets(self, nums):
-        powerSet = [[]]
-        #[1,2,3]
-        #for 1
-            #len = 1, [[], [1]]
-        #for 2
-            #len = 2, [[], [1], [2], [1,2]]
-        #for 3
-            #len = 4, [[], [1], [2], [1,2], [3], [1,3], [2,3], [1,2,3]]
-            
-        #Time and Space is O(n*2^n)
-        for num in nums:
-            length = len(powerSet)
-            for i in range(length):
-                powerSet.append(powerSet[i]+[num])
-                
+    def subsets(self, nums, start = 0, base = None, powerSet = None):
+        if powerSet is None:
+            powerSet = [[]]
+        if base is None:
+            base = []
+        if start == len(nums):
+            return powerSet
+        
+        for i in range(start, len(nums)):
+            newSet = base + [nums[i]]
+            powerSet.append(newSet)
+            self.subsets(nums, i+1, newSet, powerSet)
+        
         return powerSet
+        
+#     def subsets(self, nums):
+#         powerSet = [[]]
+#         for num in nums:
+#             length = len(powerSet)
+#             for i in range(length):
+#                 powerSet.append(powerSet[i]+[num])
+                
+#         return powerSet
         
         
