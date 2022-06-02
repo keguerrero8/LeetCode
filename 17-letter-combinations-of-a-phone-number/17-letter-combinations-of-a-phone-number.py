44b19371a939b01f05ccc0a4f499ef90a734cd6e
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-06-02 19:08:07 -0400

    Time: 39 ms (6.68%), Space: 13.6 MB (40.93%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+        if digits == "":
+            return []
+        mnemonics = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6":"mno",
+                     "7":"pqrs", "8":"tuv", "9":"wxyz"}
+        return self.phoneNumberMnemonicsHelper(0, digits, mnemonics, [], [])
+
+
+    def phoneNumberMnemonicsHelper(self, i, phoneNumber, mnemonics, base, result):
+        if i == len(phoneNumber):
+            result.append("".join(base))
+            return result
+
+        for char in mnemonics[phoneNumber[i]]:
+            new = base + [char]
+            self.phoneNumberMnemonicsHelper(i+1, phoneNumber, mnemonics, new, result)
+
+        return result
+        
