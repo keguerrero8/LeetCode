ec7bb22989708d7775793e89d98752cd35b87577
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-06-04 14:41:10 -0400

    Time: 920 ms (27.80%), Space: 29 MB (57.21%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
@@ -1,11 +1,26 @@
 class Solution(object):
     def findBuildings(self, heights):
-        stack = []
+        # stack = []
+        # for i in reversed(range(len(heights))):
+        #     if len(stack) == 0 or heights[i] > heights[stack[-1]]:
+        #         stack.append(i)
+        # result = []
+        # for i in reversed(range(len(stack))):
+        #     result.append(stack[i])
+        # return result
+        maxHeight = float("-inf")
+        output = []
         for i in reversed(range(len(heights))):
-            if len(stack) == 0 or heights[i] > heights[stack[-1]]:
-                stack.append(i)
-        result = []
-        for i in reversed(range(len(stack))):
-            result.append(stack[i])
-        return result
+            if heights[i] > maxHeight:
+                maxHeight = heights[i]
+                output.append(i)
+                
+        left, right = 0, len(output) - 1
+        
+        while left <= right:
+            output[left], output[right] = output[right], output[left]
+            left += 1
+            right -= 1
+            
+        return output
         
