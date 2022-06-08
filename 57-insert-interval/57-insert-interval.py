31d5c0018329e53e5467de002dcc0bbf05c9fb40
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-06-08 15:38:30 -0400

    Time: 118 ms (12.62%), Space: 17 MB (20.07%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -1,18 +1,16 @@
 class Solution(object):
     def insert(self, intervals, newInterval):
-        output = []
+        res = []
         
         for i in range(len(intervals)):
-            if newInterval[1] < intervals[i][0]:
-                output.append(newInterval)
-                return output + intervals[i:]
-            elif newInterval[0] > intervals[i][1]:
-                output.append(intervals[i])
+            if newInterval[0] > intervals[i][1]:
+                res.append(intervals[i])
+            elif newInterval[1] < intervals[i][0]:
+                res.append(newInterval)
+                return res + intervals[i:]
             else:
                 newInterval[0] = min(newInterval[0], intervals[i][0])
                 newInterval[1] = max(newInterval[1], intervals[i][1])
-                
-        output.append(newInterval)
-
-        return output
         
+        res.append(newInterval)
+        return res
