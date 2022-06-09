5d887eec041b699033e3b45296d40def04441e72
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-06-09 11:47:41 -0400

    Time: 472 ms (62.41%), Space: 14.3 MB (93.34%) - LeetHub

--- 55-jump-game/55-jump-game.py
+++ 55-jump-game/55-jump-game.py
@@ -1,17 +1,26 @@
 class Solution(object):
     def canJump(self, nums):
+#         if nums[0] == 0:
+#             return False
+#         jumps = [0 for i in range(len(nums))]
+#         jumps[0] = nums[0]
+        
+#         for i in range(1, len(jumps)):
+#             if jumps[i-1] - 1 >= 0:
+#                 jumps[i] = max(nums[i], jumps[i-1]-1)
+#             else:
+#                 return False
+#         return True
+    
         if len(nums) == 1:
             return True
-        
-        isPossible = False
-        IdxGoal = len(nums) - 1
-        
-        for i in reversed(range(len(nums)-1)):
-            if nums[i] + i >= IdxGoal:
-                IdxGoal = i
-                isPossible = True
+        if nums[0] == 0:
+            return False
+        prevJump = nums[0]
+        for i in range(1, len(nums)):
+            if prevJump - 1 >= 0:
+                prevJump = max(nums[i], prevJump-1)
             else:
-                isPossible = False
-        
-        return isPossible
+                return False
+        return True
         
