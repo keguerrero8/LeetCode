eb5cfcb261d2efb48f5ace79a66e500dfb68fbb0
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-06-09 13:12:48 -0400

    Time: 158 ms (11.66%), Space: 33.5 MB (23.49%) - LeetHub

--- 143-reorder-list/143-reorder-list.py
+++ 143-reorder-list/143-reorder-list.py
@@ -5,49 +5,47 @@
 #         self.next = next
 class Solution(object):
     def reorderList(self, head):
-        end = head
+        slow, right = head, head
+        
         count = 1
-        while end.next:
-            end = end.next
+        while right.next:
             count += 1
+            right = right.next
+        
         if count == 1:
             return head
-        mid = (count // 2) 
-        
-        count = 1
-        nodeToReverse = head
-        while count < mid:
-            nodeToReverse = nodeToReverse.next
-            count += 1
             
-        self.reverseList(nodeToReverse.next)
-        nodeToReverse.next = None
-        
-        start = head
-        new = newHead = ListNode(None)
-        while start or end:
-            if start:
-                new.next = start
-                start = start.next
-                new = new.next
-            new.next = end
-            end = end.next
-            new = new.next
+        slowCount = 1   
+        while slowCount < count // 2:
+            slowCount += 1
+            slow = slow.next
             
-        return newHead.next
+        self.reverseList(slow.next)
+        slow.next = None
+        
+        newList = newListHead = ListNode(None)
+        left = head
+        #1 -> 2 -> None'
+        #5 -> 4 -> 3 -> None'
+        #None -> 1 -> 5 -> 2 -> 4 -> 3
+        while left or right:
+            if left:
+                newList.next = left
+                newList = newList.next
+                left = left.next
+            newList.next = right
+            newList = newList.next
+            right = right.next
+            
+        return newListHead.next
         
         
         
     def reverseList(self, node):
         p1 = None
         p2 = node
-        
         while p2:
             p3 = p2.next
             p2.next = p1
             p1 = p2
             p2 = p3
-            
-        
-            
-        
