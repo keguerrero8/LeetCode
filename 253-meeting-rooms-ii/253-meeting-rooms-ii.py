d11f4437165384f6306ca986025b227e11a83623
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-06-13 12:08:16 -0400

    Time: 117 ms (16.56%), Space: 16.9 MB (85.88%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -2,30 +2,22 @@ class Solution(object):
     def minMeetingRooms(self, intervals):
         if len(intervals) == 1:
             return 1
-        
-        startingTimes = []
-        endTimes = []
-        
-        for interval in intervals:
-            startingTimes.append(interval[0])
-            endTimes.append(interval[1])
-            
-        startingTimes.sort()
+        startTimes = [interval[0] for interval in intervals]
+        endTimes = [interval[1] for interval in intervals]
+        startTimes.sort()
         endTimes.sort()
         
-        i = j = 0
+        start, end = 0, 0
+        rooms = 0
+        maxRooms = 0
         
-        maxRooms = 1
-        currentRooms = 0
-        while i < len(startingTimes):
-            if startingTimes[i] < endTimes[j]:
-                currentRooms += 1
-                i += 1
+        while start < len(startTimes):
+            if startTimes[start] < endTimes[end]:
+                rooms += 1
+                maxRooms = max(rooms, maxRooms)
+                start += 1
             else:
-                currentRooms -= 1
-                j += 1
-            maxRooms = max(maxRooms, currentRooms)  
+                rooms -= 1
+                end += 1
                 
         return maxRooms
-            
-        
