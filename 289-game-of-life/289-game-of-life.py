a219b0523b904a67315a44678ab4f558e0b5dfb2
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-06-13 09:42:50 -0400

    Time: 25 ms (62.13%), Space: 13.3 MB (72.77%) - LeetHub

--- 289-game-of-life/289-game-of-life.py
+++ 289-game-of-life/289-game-of-life.py
@@ -1,11 +1,5 @@
 class Solution(object):
     def gameOfLife(self, board):
-        #live = 1, dead = 0
-        #if < 2 neighbors are alive, live cell dies from 1 to 0
-        #if 2-3 neighbors are alive, live cell stays alive
-        #if > 3 neighbors are alive, live cell dies from 1 to 0
-        #if 3 neighbors are alive, dead cell lives from 0 to 1
-        
         for row in range(len(board)):
             for col in range(len(board[0])):
                 liveNeighbors = self.getNeighbors(board, row, col)
