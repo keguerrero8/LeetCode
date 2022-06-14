379287db9bd42cf0fdf650f4d4b3c37eec52d894
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-06-14 14:05:17 -0400

    Time: 73 ms (81.09%), Space: 18.5 MB (78.62%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,22 +6,22 @@
 #         self.right = right
 class Solution(object):
     def buildTree(self, preorder, inorder):
-        def buildTreeHelper(preorder, inOrderHash, preStart, preEnd, inorder, inStart, inEnd):
-            if preStart > preEnd:
-                return None
-            
-            idxInorder = inOrderHash[preorder[preStart]]
-            root = TreeNode(preorder[preStart])
-            left = idxInorder - inStart
-            
-            root.left = buildTreeHelper(preorder, inOrderHash, preStart+1, preStart+left, inorder, inStart, idxInorder)
-            root.right = buildTreeHelper(preorder, inOrderHash, preStart+1+left, preEnd, inorder, idxInorder+1, inEnd)
-            
-            return root
-        
-        inOrderHash = {}
-        for i in range(len(inorder)):
-            inOrderHash[inorder[i]] = i
-                    
-        return buildTreeHelper(preorder, inOrderHash, 0, len(preorder)-1, inorder, 0, len(inorder)-1)
+        inOrderHash = { inorder[i] : i for i in range(len(inorder))}
+        # for i in range(len(inorder)):
+        #     inOrderHash[inorder[i]] = i
+        return self.buildTreeHelper(preorder, inorder, inOrderHash, 0, len(preorder) - 1, 0, len(inorder) - 1)
     
+    
+    def buildTreeHelper(self, preorder, inorder, inOrderHash, startPre, endPre, startIn, endIn):
+        if startPre > endPre:
+            return None
+        
+        root = TreeNode(preorder[startPre])
+        inOrderIdx = inOrderHash[preorder[startPre]]
+        leftTreeLength = inOrderIdx - startIn 
+        
+        root.left = self.buildTreeHelper(preorder, inorder, inOrderHash, startPre + 1, startPre + leftTreeLength, startIn, inOrderIdx - 1)
+        root.right = self.buildTreeHelper(preorder, inorder, inOrderHash, startPre + 1 + leftTreeLength, endPre, inOrderIdx+1, endIn)
+        
+        return root
+        
