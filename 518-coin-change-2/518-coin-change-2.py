b62e802dda102cf72da7787887a275a3fde81715
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-06-16 11:36:34 -0400

    Time: 210 ms (61.83%), Space: 14 MB (46.68%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.py
+++ 518-coin-change-2/518-coin-change-2.py
@@ -2,9 +2,11 @@ class Solution(object):
     def change(self, amount, coins):
         ways = [0]*(amount+1)
         ways[0] = 1
+
         for denom in coins:
-            for target in range(1, len(ways)):
-                if denom <= target:
-                    ways[target] += ways[target-denom]
+            for i in range(1, len(ways)):
+                if denom <= i:
+                    ways[i] += ways[i-denom]
+
         return ways[-1]
         
