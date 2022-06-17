b97459aa85e4e8161d2e94ed2a41bb9402412875
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-06-17 18:54:17 -0400

    Time: 321 ms (58.97%), Space: 15.3 MB (69.31%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -1,62 +1,51 @@
 class Solution(object):
-    def pacificAtlantic(self, heights):
+    def pacificAtlantic(self, heights):        
         pac = set()
         atl = set()
         
-        #perform bfs on first row and last row
+        #iterate through first row and last row
         for col in range(len(heights[0])):
-            if (0, col) not in pac:
-                pac.add((0, col))
-            self.breadthFirstSearch(pac, 0, col, heights)
-            if (len(heights)-1, col) not in atl:
-                atl.add((len(heights)-1, col))
-            self.breadthFirstSearch(atl, len(heights)-1, col, heights)
+            self.bfs(heights, 0, col, pac)
+            self.bfs(heights, len(heights) - 1, col, atl)
             
-        #perform bfs on first col and last col
+        #iterate through first col and last col
         for row in range(len(heights)):
-            if (row, 0) not in pac:
-                pac.add((row, 0))
-            self.breadthFirstSearch(pac, row, 0, heights)
-            if (row, len(heights[0])-1) not in atl:
-                atl.add((row, len(heights[0])-1))
-            self.breadthFirstSearch(atl, row, len(heights[0])-1, heights)
+            self.bfs(heights, row, 0, pac)
+            self.bfs(heights, row, len(heights[0]) - 1, atl)
         
-        result = []
+        res = []
         for row in range(len(heights)):
             for col in range(len(heights[0])):
-                if (row, col) in pac and (row, col) in atl:
-                    result.append([row, col])
-                    
-        return result
+                if (row, col) in atl and (row, col) in pac:
+                    res.append([row, col])        
+        return res
     
     
-    def breadthFirstSearch(self, ocean, row, col, heights):
-        
+    def bfs(self, heights, row, col, ocean):
+        if (row, col) not in ocean:
+            ocean.add((row, col))
+            
         q = [[row, col]]
         
         while q:
-            cRow, cCol = q.pop(0)
-                
-            #check left
-            if cCol - 1 >= 0 and heights[cRow][cCol] <= heights[cRow][cCol-1] and (cRow, cCol - 1) not in ocean:
-                    q.append([cRow, cCol - 1])
-                    ocean.add((cRow, cCol-1))
-
-            #check right
-            if cCol + 1 < len(heights[0]) and heights[cRow][cCol] <= heights[cRow][cCol+1] and (cRow, cCol + 1) not in ocean:
-                    q.append([cRow, cCol + 1])
-                    ocean.add((cRow, cCol+1))
-
-                    
-            #check up
-            if cRow - 1 >= 0 and heights[cRow][cCol] <= heights[cRow-1][cCol] and (cRow-1, cCol) not in ocean:
-                    q.append([cRow-1, cCol])
-                    ocean.add((cRow-1, cCol))
-
+            r, c = q.pop(0)
             
+            #check left
+            if c - 1 >= 0 and (r, c - 1) not in ocean and heights[r][c-1] >= heights[r][c]:
+                ocean.add((r, c-1))
+                q.append([r, c-1])
+                
+            #check right
+            if c + 1 < len(heights[0]) and (r, c + 1) not in ocean and heights[r][c+1] >= heights[r][c]:
+                ocean.add((r, c+1))
+                q.append([r, c+1])
+                
+            #check up
+            if r - 1 >= 0 and (r - 1, c) not in ocean and heights[r-1][c] >= heights[r][c]:
+                ocean.add((r-1, c))
+                q.append([r-1, c])
+                
             #check down
-            if cRow + 1 < len(heights) and heights[cRow][cCol] <= heights[cRow+1][cCol] and (cRow+1, cCol) not in ocean:
-                    q.append([cRow+1, cCol])
-                    ocean.add((cRow+1, cCol))
-
-        
+            if r + 1 < len(heights) and (r + 1, c) not in ocean and heights[r+1][c] >= heights[r][c]:
+                ocean.add((r+1, c))
+                q.append([r+1, c])
