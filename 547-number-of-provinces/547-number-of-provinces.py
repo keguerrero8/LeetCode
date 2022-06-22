6440da36e697a106aed0645bbc89e5a15e045b82
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-06-22 12:44:15 -0400

    Time: 194 ms (68.65%), Space: 13.9 MB (45.77%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.py
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -1,36 +1,57 @@
 class Solution(object):
     def findCircleNum(self, isConnected):
-        provinces = len(isConnected)
-        parent = [i for i in range(provinces)]
-        rank = [1 for i in range(provinces)]
+        visited = set()
+        provinces = 0
         
-        def find(n):
-            res = n
-            while res != parent[res]:
-                parent[res] = parent[parent[res]]
-                res = parent[res]
-                
-            return res
-                
-        
-        def union(n1, n2):
-            p1, p2 = find(n1), find(n2)
-            if p1 == p2: return 0
+        def dfs(city):
+            visited.add(city)
+            for neighbor in range(len(isConnected[city])):
+                if neighbor not in visited and isConnected[city][neighbor] == 1 and city != neighbor:
+                    dfs(neighbor)
+
             
-            if rank[p1] > rank[p2]:
-                parent[p2] = p1
-                rank[p1] += rank[p2]
-            else:
-                parent[p1] = p2
-                rank[p2] += p1
+        for city in range(len(isConnected)):
+            if city not in visited:
+                dfs(city)
+                provinces += 1
                 
-            return 1
+        return provinces
+            
+            
+            
+            
+#         provinces = len(isConnected)
+#         parent = [i for i in range(provinces)]
+#         rank = [1 for i in range(provinces)]
         
-        for i in range(len(isConnected)):
-            for j in range(len(isConnected[i])):
-                if i != j and isConnected[i][j] == 1:
-                    provinces -= union(i, j)
+#         def find(n):
+#             res = n
+#             while res != parent[res]:
+#                 parent[res] = parent[parent[res]]
+#                 res = parent[res]
+                
+#             return res
+                
+        
+#         def union(n1, n2):
+#             p1, p2 = find(n1), find(n2)
+#             if p1 == p2: return 0
+            
+#             if rank[p1] > rank[p2]:
+#                 parent[p2] = p1
+#                 rank[p1] += rank[p2]
+#             else:
+#                 parent[p1] = p2
+#                 rank[p2] += p1
+                
+#             return 1
+        
+#         for i in range(len(isConnected)):
+#             for j in range(len(isConnected[i])):
+#                 if i != j and isConnected[i][j] == 1:
+#                     provinces -= union(i, j)
         
                     
-        return provinces
+#         return provinces
+    
         
