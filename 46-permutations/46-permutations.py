7225bfd85281f277e8959b418839693e8110ff4a
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-06-23 17:20:52 -0400

    Time: 69 ms (5.16%), Space: 13.6 MB (64.35%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,18 +1,17 @@
 class Solution(object):
-    def permute(self, nums):
-        result = []
-        return self.getPermutationsHelper(nums, 0, result)
-
-
-    def getPermutationsHelper(self, array, idx, result):
-        if idx == len(array) - 1:
-            result.append(array[:])
-            return result
-
-        for i in range(idx, len(array)):
-            array[idx], array[i] = array[i], array[idx]
-            self.getPermutationsHelper(array, idx + 1, result)
-            array[idx], array[i] = array[i], array[idx]
-
-        return result
+    def permute(self, nums, res = None, start = 0):
+        if res == None:
+            res = []
+            
+        if start > len(nums) - 1:
+            res.append(nums + [])
+            return
+        
+        for i in range(start, len(nums)):
+            nums[start], nums[i] = nums[i], nums[start]
+            self.permute(nums, res, start + 1)
+            nums[start], nums[i] = nums[i], nums[start]
+            
+        return res
+        
         
