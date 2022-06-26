02b89fd5d81ba258aff8137afdfdb20be75cf994
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-06-26 15:55:07 -0400

    Time: 51 ms (6.68%), Space: 13.5 MB (43.26%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -1,21 +1,19 @@
 class Solution(object):
     def rotate(self, matrix):
-        L = 0
-        R = len(matrix) - 1
-        top = 0
-        bottom = len(matrix) - 1
+        L, R = 0, len(matrix) - 1
+        T, B = 0, len(matrix) - 1
         
-        while L <= R or top <= bottom:
+        while L < R:
             for i in range(R-L):
-                temp = matrix[top][L+i]
-                matrix[top][L+i] = matrix[bottom-i][L]
-                matrix[bottom-i][L] = matrix[bottom][R-i]
-                matrix[bottom][R-i] = matrix[top+i][R]
-                matrix[top+i][R] = temp
+                temp = matrix[T][L+i]
+                matrix[T][L+i] = matrix[B-i][L]
+                matrix[B-i][L] = matrix[B][R-i]
+                matrix[B][R-i] = matrix[T+i][R]
+                matrix[T+i][R] = temp
             L += 1
             R -= 1
-            top += 1
-            bottom -= 1
+            T += 1
+            B -= 1
             
         return matrix
         
