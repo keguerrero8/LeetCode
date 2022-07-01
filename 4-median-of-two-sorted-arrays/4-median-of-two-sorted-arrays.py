98bafe09bbe131b2db087939c38222c8afbe025c
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-07-01 12:12:49 -0400

    Time: 202 ms (11.19%), Space: 14.2 MB (25.10%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -1,30 +1,30 @@
-class Solution(object):
-    def findMedianSortedArrays(self, nums1, nums2):
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         A, B = nums1, nums2
-        total = len(nums1) + len(nums2)
+        total = len(A) + len(B)
         half = total // 2
         
         if len(B) < len(A):
             A, B = B, A
-        
-        l, r = 0, len(A) - 1
+            
+        l, r = 0, len(A)-1
         
         while True:
-            midA = (l + r) // 2 #0 -> 0
-            midB = half - midA - 2 #2-0-2 = 0
+            midA = (l+r)//2
+            midB = half - midA - 2
             
-            Aleft = A[midA] if midA >= 0 else float("-inf") #1
-            Aright = A[midA+1] if (midA + 1) < len(A) else float("inf") #2
-            Bleft = B[midB] if midB >= 0 else float("-inf") #3
-            Bright = B[midB+1] if (midB + 1) < len(B) else float("inf") #4
+            leftA = A[midA] if midA >= 0 else float("-inf")
+            rightA = A[midA+1] if midA + 1 < len(A) else float("inf")
+            leftB = B[midB] if midB >= 0 else float("-inf")
+            rightB = B[midB+1] if midB+1 < len(B) else float("inf")
             
-            if Aleft <= Bright and Bleft <= Aright:
-                if total % 2 == 0:
-                    return (float(max(Aleft, Bleft)) + float(min(Aright, Bright))) / 2
+            if leftA <= rightB and leftB <= rightA:
+                if total % 2:
+                    return min(rightB, rightA)
                 else:
-                    return min(Aright, Bright)
-            elif Aleft > Bright:
+                    return (max(leftA, leftB) + min(rightB, rightA)) / 2
+            elif leftA > rightB:
                 r = midA - 1
             else:
-                l = midA + 1 #0+1 = 1
-            
+                l = midA + 1
+                
