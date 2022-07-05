34717ee30a84979812b84a6bc540f927f3c5fbff
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-07-04 20:12:45 -0400

    Time: 63 ms (87.90%), Space: 16.8 MB (47.65%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -1,16 +1,29 @@
 class Solution(object):
     def insert(self, intervals, newInterval):
-        res = []
+        mergedInterval = []
         
         for i in range(len(intervals)):
-            if newInterval[0] > intervals[i][1]:
-                res.append(intervals[i])
-            elif newInterval[1] < intervals[i][0]:
-                res.append(newInterval)
-                return res + intervals[i:]
+            if newInterval[1] < intervals[i][0]:
+                mergedInterval.append(newInterval)
+                mergedInterval = mergedInterval + intervals[i:]
+                return mergedInterval
+            elif newInterval[0] <= intervals[i][1]:
+                mergedInterval.append([min(intervals[i][0], newInterval[0]), max(intervals[i][1], newInterval[1])])
+                j = i+1
+                while j < len(intervals) and intervals[j][0] <= mergedInterval[-1][1]:
+                    mergedInterval[-1] = [min(mergedInterval[-1][0], intervals[j][0]), max(mergedInterval[-1][1], intervals[j][1])]
+                    j += 1
+                if j == len(intervals):
+                    return mergedInterval
+                else:
+                    mergedInterval = mergedInterval + intervals[j:]
+                    return mergedInterval 
             else:
-                newInterval[0] = min(newInterval[0], intervals[i][0])
-                newInterval[1] = max(newInterval[1], intervals[i][1])
+                mergedInterval.append(intervals[i])
+                
+        mergedInterval.append(newInterval)
+                
+        return mergedInterval
+            
+            
         
-        res.append(newInterval)
-        return res
