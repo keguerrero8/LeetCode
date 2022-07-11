84f71b3c11fc1bb30d9477de0098c70cb2954df0
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-07-11 10:43:16 -0400

    Time: 127 ms (42.95%), Space: 14.7 MB (91.36%) - LeetHub

--- 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
@@ -3,27 +3,31 @@ class Solution(object):
         parent = [i for i in range(n)]
         rank = [1 for i in range(n)]
         
-        def find(node):
-            res = node
-            
-            while res != parent[res]:
-                parent[res] = parent[parent[res]]
-                res = parent[res]
-                
-            return res
+        def find(n):
+            cur = n
+            while cur != parent[cur]:
+                parent[cur] = parent[parent[cur]]
+                cur = parent[cur]
+            return cur
         
         def union(n1, n2):
             p1, p2 = find(n1), find(n2)
+            
             if p1 == p2: return 0
+            
             if rank[p2] > rank[p1]:
-                parent[p1] = p2
                 rank[p2] += rank[p1]
+                parent[p1] = p2
             else:
-                parent[p2] = p1
                 rank[p1] += rank[p2]
-            return 1 
+                parent[p2] = p1
+                
+            return 1
         
-        res = n   
-        for x, y in edges:
-            res -= union(x, y)
-        return res
+        components = n
+        for node1, node2 in edges:
+            components -= union(node1, node2)
+            
+        return components
+                
+        
