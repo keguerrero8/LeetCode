cb4213986b592dd242b6e6c4b1e49fd9449a3073
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-07-12 13:14:50 -0400

    Time: 786 ms (66.17%), Space: 13.7 MB (16.40%) - LeetHub

--- 18-4sum/18-4sum.py
+++ 18-4sum/18-4sum.py
@@ -1,29 +1,39 @@
 class Solution(object):
     def fourSum(self, nums, target):
         nums.sort()
-        res, quad = [], []
+        res = []
+        base = []
         
-        def kSum(k, start, target):
-            if k != 2:
-                for i in range(start, len(nums) - k + 1):
-                    if i > start and nums[i] == nums[i-1]:
-                        continue
-                    quad.append(nums[i])
-                    kSum(k - 1, i + 1, target - nums[i])
-                    quad.pop()
+        def findSumCombinations(num, start, target):
+            if num == 2:
+                self.twoSum(start, res, base, nums, target)
                 return
             
-            l, r = start, len(nums) - 1
-            while l < r:
-                if nums[l] + nums[r] < target:
+            end = len(nums) - num + 1
+            for i in range(start, end):
+                if i > start and nums[i] == nums[i-1]:
+                    continue
+                newSum = target - nums[i]
+                base.append(nums[i])
+                findSumCombinations(num - 1, i + 1, newSum)
+                base.pop()
+            return res
+        
+        return findSumCombinations(4, 0, target)
+    
+    def twoSum(self, start, res, base, nums, target):
+        l, r = start, len(nums) - 1
+        
+        while l < r:
+            total = nums[l] + nums[r]
+            if total == target:
+                res.append(base + [nums[l], nums[r]])
+                l += 1
+                while l < r and nums[l] == nums[l-1]:
                     l += 1
-                elif nums[l] + nums[r] > target:
-                    r -= 1
-                else:
-                    res.append(quad + [nums[l], nums[r]])
-                    l += 1
-                    while l < r and nums[l] == nums[l-1]:
-                        l += 1
-        kSum(4,0,target)
-        return res   
+            elif total < target:
+                l += 1
+            else:
+                r -= 1
+    
         
