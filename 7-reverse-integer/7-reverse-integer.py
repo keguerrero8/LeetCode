2925a3f968a7120ad9213be014814aa993991b96
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-07-17 14:09:52 -0400

    Time: 29 ms (59.74%), Space: 13.4 MB (64.25%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.py
+++ 7-reverse-integer/7-reverse-integer.py
@@ -5,16 +5,18 @@ class Solution(object):
             isNegative = True
             
         MAXINT = 2147483647
-        MININT = -2147483648
+        MININT = 2147483648
         x = abs(x)
         res = 0
         
         while x != 0:
             rem = x % 10
-            if res > MAXINT//10 or res == MAXINT//10 and rem > 7:
+            if res > MAXINT//10 or res == MAXINT//10 and rem >= 7:
                 return 0
-            else:
-                res = res*10 + rem
+            elif isNegative:
+                if res > MININT//10 or res == MININT//10 and rem >= 8:
+                    return 0
+            res = res*10 + rem
             x = x // 10
             
         return res if not isNegative else -1*res
