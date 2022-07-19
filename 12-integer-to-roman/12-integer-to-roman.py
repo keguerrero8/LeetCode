5b8be1147330eaf2f4f77fbf80ac3609daeff9c5
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-07-19 16:26:39 -0400

    Time: 61 ms (51.87%), Space: 13.6 MB (15.34%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.py
+++ 12-integer-to-roman/12-integer-to-roman.py
@@ -1,19 +1,26 @@
 class Solution(object):
     def intToRoman(self, num):
-        symbols = [[1, "I"], [4, "IV"], [5, "V"], [9, "IX"], [10, "X"], [40, "XL"], [50, "L"], [90, "XC"], [100, "C"], [400, "CD"], [500, "D"], [900, "CM"], [1000, "M"]]
-        intToRoman = []
-        while num:
-            for i in reversed(range(len(symbols))):
-                if num >= symbols[i][0]:
-                    intToRoman.append(symbols[i][1])
-                    divisor = symbols[i][0]
+        romanNums = [[1000, "M"], [900, "CM"], [500, "D"], [400, "CD"], [100, "C"], [90, "XC"], [50, "L"], [40, "XL"], [10, "X"], [9, "IX"], [5, "V"], [4, "IV"], [1, "I"]]
+        res = []
+        #search from beginning of array until current num > arrayVal
+        #current num // arrayVal gives us how many times to use that char so append x times to out array
+        #take modulos of current num % arrayVal to get new
+        
+        while num != 0:
+            i = 0
+            while i < len(romanNums):
+                if num >= romanNums[i][0]:
+                    rn = romanNums[i]
                     break
+                i += 1
                     
-            num -= divisor
-        
-        #1994, ["M", ], 1994 - 1000 = 994
-        #994, ["M", "CM", ], 994 - 900 = 94
-        #94, ["M", "CM", "XC",], 94 - 90 = 4
-        #4, ["M", "CM", "XC", "IV"], 4-4 = 0
-        return "".join(intToRoman)
-        
+            amountOfChars = num // rn[0]
+            for i in range(amountOfChars):
+                res.append(rn[1])
+                
+            num = num % rn[0]
+            
+            
+        return "".join(res)
+                    
+                
