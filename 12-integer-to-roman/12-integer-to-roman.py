6781d9a6370c3ab83ca0f75934a82b1a5ea68e28
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-07-19 16:27:25 -0400

    Time: 53 ms (66.16%), Space: 13.5 MB (40.20%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.py
+++ 12-integer-to-roman/12-integer-to-roman.py
@@ -2,9 +2,6 @@ class Solution(object):
     def intToRoman(self, num):
         romanNums = [[1000, "M"], [900, "CM"], [500, "D"], [400, "CD"], [100, "C"], [90, "XC"], [50, "L"], [40, "XL"], [10, "X"], [9, "IX"], [5, "V"], [4, "IV"], [1, "I"]]
         res = []
-        #search from beginning of array until current num > arrayVal
-        #current num // arrayVal gives us how many times to use that char so append x times to out array
-        #take modulos of current num % arrayVal to get new
         
         while num != 0:
             i = 0
