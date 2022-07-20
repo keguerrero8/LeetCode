acac6563897a2f3c2492c472d9c6be3ad97bdd4e
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-07-20 14:11:17 -0400

    Time: 161 ms (38.13%), Space: 14.3 MB (25.44%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -1,30 +1,30 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        A, B = nums1, nums2
-        total = len(A) + len(B)
+        total = len(nums1) + len(nums2)
         half = total // 2
+        A, B = nums1, nums2
         
         if len(B) < len(A):
             A, B = B, A
             
-        l, r = 0, len(A)-1
+        l, r = 0, len(A) - 1
         
         while True:
-            midA = (l+r)//2
+            midA = (l + r) // 2
             midB = half - midA - 2
             
-            leftA = A[midA] if midA >= 0 else float("-inf")
-            rightA = A[midA+1] if midA + 1 < len(A) else float("inf")
-            leftB = B[midB] if midB >= 0 else float("-inf")
-            rightB = B[midB+1] if midB+1 < len(B) else float("inf")
+            Aleft = A[midA] if midA >= 0 else float("-inf")
+            Aright = A[midA + 1] if midA + 1 < len(A) else float("inf")
+            Bleft = B[midB] if midB >= 0 else float("-inf")
+            Bright = B[midB + 1] if midB + 1 < len(B) else float("inf")
             
-            if leftA <= rightB and leftB <= rightA:
-                if total % 2:
-                    return min(rightB, rightA)
+            if Aleft <= Bright and Bleft <= Aright:
+                if total % 2: 
+                    return min(Aright, Bright)
                 else:
-                    return (max(leftA, leftB) + min(rightB, rightA)) / 2
-            elif leftA > rightB:
-                r = midA - 1
-            else:
+                    return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
+                    
+            elif Bleft > Aright:
                 l = midA + 1
-                
+            else:
+                r = midA - 1
