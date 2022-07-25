b32492abc4f425a6cd3ed0311e2ee384e5789a94
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-07-25 11:41:12 -0400

    Time: 219 ms (11.47%), Space: 14.8 MB (95.87%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -1,19 +1,32 @@
 class Solution(object):
-    def trap(self, height):
-        L = [0 for i in range(len(height))]
-        R = [0 for i in range(len(height))]
-        
-        for i in range(1, len(height)):
-            L[i] = max(L[i-1], height[i-1])
-            
-        for i in reversed(range(len(height)-1)):
-            R[i] = max(R[i+1], height[i+1])
-            
-        waterArea = 0
-        
-        for i in range(len(height)):
-            minHeight = min(R[i], L[i])
-            trappedWater = minHeight - height[i]
-            waterArea += trappedWater if trappedWater > 0 else 0
-            
-        return waterArea
+    def trap(self, heights):
+        if len(heights) == 0:
+            return 0
+
+        l, r = 0, len(heights) - 1
+        leftMax = [heights[l], l]
+        rightMax = [heights[r], r]
+        totalArea = 0
+
+        while l < r:
+
+            if heights[l] > leftMax[0]:
+                leftMax = [heights[l], l]
+            if heights[r] > rightMax[0]:
+                rightMax = [heights[r], r]
+
+            if l != leftMax[1]:
+                area = min(leftMax[0], rightMax[0]) - heights[l]
+            elif r != rightMax[1]:
+                area = min(leftMax[0], rightMax[0]) - heights[r]
+            else:
+                area = 0
+
+            totalArea += area
+
+            if heights[l] < heights[r]:
+                l += 1
+            else:
+                r -= 1
+
+        return totalArea
