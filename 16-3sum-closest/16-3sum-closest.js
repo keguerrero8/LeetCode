067dc256676543324aa053ec4b4ff7aa30f556d6
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-07-28 11:39:37 -0400

    Time: 204 ms (54.65%), Space: 44.5 MB (52.52%) - LeetHub

---
+++ 16-3sum-closest/16-3sum-closest.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function(nums, target) {
+    let res = Infinity
+    let delta = Infinity
+    nums.sort(function(a, b) {
+        return a - b;
+    })
+    
+    for (let i = 0; i < nums.length - 2; i++) {
+        let l = i + 1
+        let r = nums.length - 1
+        while (l < r) {
+            let currentSum = nums[i] + nums[l] + nums[r]
+            let currentDelta = Math.abs(target - currentSum)
+            if (currentDelta < delta) {
+                res = currentSum
+                delta = currentDelta
+            }
+            if (currentSum === target) {
+                return res
+            } else if (currentSum < target) {
+                l += 1
+            } else {
+                r -= 1
+            }  
+        }
+        
+    }
+    return res 
+};
