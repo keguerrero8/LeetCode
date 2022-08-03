6e571b85dbdfc88c8075997b626a3b053fdb3ee6
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-08-03 08:29:27 -0400

    Time: 1932 ms (8.96%), Space: 25.7 MB (7.42%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,11 +1,29 @@
 class Solution(object):
     def findKthLargest(self, nums, k):
-        for i in range(len(nums)):
-            nums[i] *= -1
-        heapify(nums)
-        
-        for i in range(k):
-            kthLargest = heapq.heappop(nums)
-            
-        return kthLargest*-1
+        return self.quickSelectHelper(nums, k, 0, len(nums) - 1)
+
+    def quickSelectHelper(self, array, k, start, end):
+        partitionIdx = self.partition(array, start, end)
+
+        if partitionIdx + 1 == k:
+            return array[partitionIdx]
+        elif partitionIdx + 1 > k:
+            return self.quickSelectHelper(array, k, start, partitionIdx - 1)
+        else:
+            return self.quickSelectHelper(array, k, partitionIdx + 1, end)
+
+    def partition(self, array, start, end):
+        pivot = array[end]
+
+        i = start - 1
+        j = start
+
+        while j < end:
+            if array[j] >= pivot:
+                i += 1
+                array[j], array[i] = array[i], array[j]
+            j += 1
+
+        array[end], array[i+1] = array[i+1], array[end]
+        return i + 1
         
