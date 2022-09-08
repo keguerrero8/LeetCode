24239f676505b83c4c487b47857e6998355c2731
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-09-08 10:19:20 -0600

    Time: 154 ms (21.58%), Space: 14.6 MB (75.06%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.py
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -1,18 +1,32 @@
 class Solution(object):
     def minPathSum(self, grid):
-        pathSum = [[0 for col in range(len(grid[0]))] for row in range(len(grid))]
-        
-        for row in range(len(pathSum)):
-            for col in range(len(pathSum[0])):
-                left = pathSum[row][col - 1] if col - 1 >= 0 else float("inf")
-                up = pathSum[row - 1][col] if row - 1 >= 0 else float("inf")
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                left = grid[row][col - 1] if col - 1 >= 0 else float("inf")
+                up = grid[row - 1][col] if row - 1 >= 0 else float("inf")
                 
                 if up == float("inf") and left == float("inf"):
                     minPath = 0
                 else:
                     minPath = min(left, up)
                 
-                pathSum[row][col] = grid[row][col] + minPath
+                grid[row][col] = grid[row][col] + minPath
                 
-        return pathSum[-1][-1]
+        return grid[-1][-1]
+    
+#         pathSum = [[0 for col in range(len(grid[0]))] for row in range(len(grid))]
+        
+#         for row in range(len(pathSum)):
+#             for col in range(len(pathSum[0])):
+#                 left = pathSum[row][col - 1] if col - 1 >= 0 else float("inf")
+#                 up = pathSum[row - 1][col] if row - 1 >= 0 else float("inf")
+                
+#                 if up == float("inf") and left == float("inf"):
+#                     minPath = 0
+#                 else:
+#                     minPath = min(left, up)
+                
+#                 pathSum[row][col] = grid[row][col] + minPath
+                
+#         return pathSum[-1][-1]
         
