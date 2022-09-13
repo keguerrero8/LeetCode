7a2cf48f9742d4f5ad23058b29d183509158f65c
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-09-13 15:04:12 -0400

    Time: 87 ms (19.70%), Space: 13.5 MB (40.01%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.py
+++ 12-integer-to-roman/12-integer-to-roman.py
@@ -1,6 +1,5 @@
 class Solution(object):
     def intToRoman(self, num):
-        #1994
         romans = [[1, "I"], [4, "IV"], [5, "V"], [9, "IX"], [10, "X"], [40, "XL"], [50, "L"], [90, "XC"], [100, "C"], [400, "CD"], [500, "D"], [900, "CM"], [1000, "M"]]
         
         res = []
