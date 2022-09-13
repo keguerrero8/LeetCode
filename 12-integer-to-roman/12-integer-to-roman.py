001a90b3e95f7d4c4c670d77020b3319d88ab470
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-09-13 15:03:54 -0400

    Time: 84 ms (22.38%), Space: 13.5 MB (14.85%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.py
+++ 12-integer-to-roman/12-integer-to-roman.py
@@ -1,23 +1,20 @@
 class Solution(object):
     def intToRoman(self, num):
-        romanNums = [[1000, "M"], [900, "CM"], [500, "D"], [400, "CD"], [100, "C"], [90, "XC"], [50, "L"], [40, "XL"], [10, "X"], [9, "IX"], [5, "V"], [4, "IV"], [1, "I"]]
+        #1994
+        romans = [[1, "I"], [4, "IV"], [5, "V"], [9, "IX"], [10, "X"], [40, "XL"], [50, "L"], [90, "XC"], [100, "C"], [400, "CD"], [500, "D"], [900, "CM"], [1000, "M"]]
+        
         res = []
         
-        while num != 0:
-            i = 0
-            while i < len(romanNums):
-                if num >= romanNums[i][0]:
-                    rn = romanNums[i]
-                    break
-                i += 1
-                    
-            amountOfChars = num // rn[0]
-            for i in range(amountOfChars):
-                res.append(rn[1])
-                
-            num = num % rn[0]
-            
+        i = len(romans) - 1
+        while num:
+            if num >= romans[i][0]:
+                j = num // romans[i][0]
+                num = num % romans[i][0]
+                k = 0
+                while k < j:
+                    res.append(romans[i][1])
+                    k += 1
+            i -= 1
             
         return "".join(res)
-                    
-                
+        
