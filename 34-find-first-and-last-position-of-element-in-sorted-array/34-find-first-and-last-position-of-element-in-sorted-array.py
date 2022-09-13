e2c3baaf6041a20e96486e47fbc8904685bded95
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-09-13 17:03:21 -0400

    Time: 168 ms (21.59%), Space: 15.5 MB (47.94%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,28 +1,22 @@
-class Solution(object):
-    def searchRange(self, nums, target):
-        if len(nums) == 0:
-            return [-1, -1]
-        
-        left = self.binSearch(nums, target, True)
-        right = self.binSearch(nums, target, False)
-        
-        return [left, right]
-        
-    def binSearch(self, nums, target, leftBias):
-        l, r = 0, len(nums) - 1
-        i = -1
-        while l <= r:
-            m = (l + r) // 2
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:                
+        def bSearch(leftBias):
+            i = -1
+            l, r = 0, len(nums) - 1
             
-            if target > nums[m]:
-                l = m + 1
-            elif target < nums[m]:
-                r = m - 1
-            else:
-                i = m
-                if leftBias:
-                    r = m - 1
+            while l <= r:
+                mid = (l+r) // 2
+                if target > nums[mid]:
+                    l = mid + 1
+                elif target < nums[mid]:
+                    r = mid - 1
                 else:
-                    l = m + 1
-        return i
-                
+                    i = mid
+                    if leftBias:
+                        r = mid - 1
+                    else:
+                        l = mid + 1
+            return i
+        left = bSearch(True)
+        right = bSearch(False)
+        return [left, right]
