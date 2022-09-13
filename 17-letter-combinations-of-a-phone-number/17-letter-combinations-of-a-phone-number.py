7da94d0e6dd2fcea3a0a749d1111cc2430922da7
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-09-13 17:25:30 -0400

    Time: 62 ms (16.06%), Space: 14 MB (31.56%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -1,21 +1,19 @@
-class Solution(object):
-    def letterCombinations(self, digits):
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
         if digits == "":
             return []
-        mnemonics = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6":"mno",
-                     "7":"pqrs", "8":"tuv", "9":"wxyz"}
-        return self.phoneNumberMnemonicsHelper(0, digits, mnemonics, [], [])
-
-
-    def phoneNumberMnemonicsHelper(self, i, phoneNumber, mnemonics, base, result):
-        if i == len(phoneNumber):
-            result.append("".join(base))
-            return result
-
-        for char in mnemonics[phoneNumber[i]]:
-            base.append(char)
-            self.phoneNumberMnemonicsHelper(i+1, phoneNumber, mnemonics, base, result)
-            base.pop()
-
-        return result
+        combos = {"2":"abc", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs",
+        "8":"tuv", "9":"wxyz"}
+        res = []
+        self.comboHelper(digits, combos, [], res, 0)
+        return res
+    
+    def comboHelper(self, digits, combos, combo, res, idx):
+        if idx == len(digits):
+            res.append("".join([] + combo))
+            return
         
+        for letter in combos[digits[idx]]:
+            combo.append(letter)
+            self.comboHelper(digits, combos, combo, res, idx + 1)
+            combo.pop()
