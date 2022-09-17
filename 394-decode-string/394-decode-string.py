5e0e0c0f3ed9bd09144bf4c89cb60278f317fd6d
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-09-17 13:37:25 -0400

    Time: 32 ms (26.97%), Space: 13.9 MB (5.64%) - LeetHub

--- 394-decode-string/394-decode-string.py
+++ 394-decode-string/394-decode-string.py
@@ -1,28 +1,47 @@
 class Solution(object):
     def decodeString(self, s):
-        st, pt = self.decodeHelper(s, 0)
-        return st
-    
-    def decodeHelper(self, s, i):
-        res = []
+        stack = []
         
-        while i < len(s) and s[i] != "]":
-            if s[i] in "abcdefghijklmnopqrstuvwxyz":
-                res.append(s[i])
+        for i in range(len(s)):
+            if s[i] != "]":
+                stack.append(s[i])
             else:
-                start = i
-                while s[i+1] != "[":
-                    i += 1
-                x = 0
-                partString, j = self.decodeHelper(s, i+2)
-                while x < int(s[start:i+1]):
-                    res.append(partString)
-                    x += 1
-                i = j
-            i += 1
+                substr = ""
+                while stack[-1] != "[":
+                    substr = stack.pop() + substr
+                stack.pop()
+                
+                k = ""
+                while stack and stack[-1].isdigit():
+                    k = stack.pop() + k
+                stack.append(int(k)*substr)
+                
+        return "".join(stack)
+    
+    
+#     def decodeString(self, s):
+#         st, pt = self.decodeHelper(s, 0)
+#         return st
+    
+#     def decodeHelper(self, s, i):
+#         res = []
+        
+#         while i < len(s) and s[i] != "]":
+#             if s[i] in "abcdefghijklmnopqrstuvwxyz":
+#                 res.append(s[i])
+#             else:
+#                 start = i
+#                 while s[i+1] != "[":
+#                     i += 1
+#                 x = 0
+#                 partString, j = self.decodeHelper(s, i+2)
+#                 while x < int(s[start:i+1]):
+#                     res.append(partString)
+#                     x += 1
+#                 i = j
+#             i += 1
             
-        #[acc, acc, acc]
-        return "".join(res), i
+#         return "".join(res), i
             
         
                     
