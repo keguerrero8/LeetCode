83e2eb322faca60619f1d22b33adba8006c8bbb9
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-09-19 14:53:29 -0400

    Time: 36 ms, Memory: 13.3 MB - LeetHub

---
+++ reverse-integer/reverse-integer.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def reverse(self, x):
+        isNegative = False
+        if x < 0:
+            isNegative = True
+            
+        MAXINT = 2147483647
+        MININT = 2147483648
+        x = abs(x)
+        res = 0
+        
+        while x != 0:
+            rem = x % 10
+            if res > MAXINT//10 or res == MAXINT//10 and rem >= 7:
+                return 0
+            elif isNegative:
+                if res > MININT//10 or res == MININT//10 and rem >= 8:
+                    return 0
+            res = res*10 + rem
+            x = x // 10
+            
+        return res if not isNegative else -1*res
