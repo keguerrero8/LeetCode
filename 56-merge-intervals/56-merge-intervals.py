7f88e4634b648721bc4f09ddb62256b27814b225
Kevin Guerrero <91296112+keguerrero8@users.noreply.github.com>
2022-09-20 11:05:54 -0400

    Time: 283 ms (11.45%), Space: 18.5 MB (33.19%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def merge(self, intervals):
-        intervals.sort(key = lambda x: x[0])
+        intervals.sort()
         mergedIntervals = [intervals[0]]
         for i in range(1, len(intervals)):
             if intervals[i][0] <= mergedIntervals[-1][1]:
